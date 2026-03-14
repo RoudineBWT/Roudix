ece5193b8936fd892e91750914ff3e0041f3bfa5
roudinebwt <roudinebwt@proton.me>
2026-03-15 00:42:24 +0100

    Adding flake update and cleanup alias

--- modules/fish.nix
+++ modules/fish.nix
@@ -4,7 +4,14 @@
   programs.fish = {
     enable = true;
     shellAliases = {
+      update  = "sudo nix flake update";
       rebuild = "sudo nixos-rebuild switch --flake /home/roudine/.config/roudix#roudix";
+      cleanup = "sudo nix-env --delete-generations +3 --profile /nix/var/nix/profiles/system && sudo nix-collect-garbage";
     };
   };
+
+  programs.starship = {
+      enable = true;
+      enableFishIntegration = true;
+    };
 }
