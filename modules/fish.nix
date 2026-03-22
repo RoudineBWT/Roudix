0e85f1c0e57cd9862195d2b4915763722e2bc0ed
roudinebwt <roudinebwt@proton.me>
2026-03-22 20:46:14 +0100

    feat(fish): add confirmation to the alias

--- modules/fish.nix
+++ modules/fish.nix
@@ -4,8 +4,8 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      update = "sudo nix flake update --flake $NH_FLAKE && nh os switch $NH_FLAKE";
-      rebuild = "nh os switch $NH_FLAKE";
+      update = "sudo nix flake update --flake $NH_FLAKE && nh os switch --accept-flake-config $NH_FLAKE";
+      rebuild = "nh os switch --accept-flake-config $NH_FLAKE";
       cleanup = "sudo nix-env --delete-generations +3 --profile /nix/var/nix/profiles/system && sudo nix-collect-garbage";
     };
   };
