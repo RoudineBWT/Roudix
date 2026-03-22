81b877e2fd39c8dc6d4f78190c0a6be623cf50de
roudinebwt <roudinebwt@proton.me>
2026-03-22 01:02:13 +0100

    fix(fish): add --flake flag to nix flake update alias

--- modules/fish.nix
+++ modules/fish.nix
@@ -4,8 +4,8 @@
   programs.fish = {
     enable = true;
     shellAliases = {
-      update = "sudo nix flake update $FLAKE && nh os switch $FLAKE";
-      rebuild = "nh os switch $FLAKE";
+      update = "sudo nix flake update --flake $NH_FLAKE && nh os switch $NH_FLAKE";
+      rebuild = "nh os switch $NH_FLAKE";
       cleanup = "sudo nix-env --delete-generations +3 --profile /nix/var/nix/profiles/system && sudo nix-collect-garbage";
     };
   };
