e5a040d56cc223fe90f8cd2fcd00bd4f7da2387d
roudinebwt <roudinebwt@proton.me>
2026-03-24 02:01:52 +0100

    feat(flake): add noctalia's key for binary and update flake

--- flake.nix
+++ flake.nix
@@ -5,10 +5,12 @@
     extra-substituters = [
       "https://attic.xuyh0120.win/lantian"
       "https://cache.garnix.io"
+      "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
 
