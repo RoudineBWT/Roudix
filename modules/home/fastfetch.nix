d5b0f01eba274000fd9a1c31555c4c673dec85f5
roudinebwt <roudinebwt@proton.me>
2026-04-10 22:28:34 +0200

    the logo in fastfetch follow the colors schemes

--- modules/home/fastfetch.nix
+++ modules/home/fastfetch.nix
@@ -8,8 +8,9 @@
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
 
       logo = {
-        type = "file";
+        type = "file-raw";
         source = "${dotfiles}/fastfetch/roudix.txt";
+        height = 20;
       };
 
       display = {
