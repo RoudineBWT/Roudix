ab0c1747424e4f591161342f8d5135fca8fa235e
roudinebwt <roudinebwt@proton.me>
2026-04-11 00:55:41 +0200

    add default wallpaper for gnome and kde, logo menu for plasma

--- branding.nix
+++ branding.nix
@@ -8,8 +8,9 @@ in
     roudix-branding
   ];
 
-  environment.pathsToLink = [ "/share/icons" ];
+  environment.pathsToLink = [ "/share/icons" "/share/backgrounds" "/share/gnome-background-properties" ];
 
+  # Logo GDM
   programs.dconf.profiles.gdm.databases = [{
     settings = {
       "org/gnome/login-screen" = {
@@ -17,4 +18,36 @@ in
       };
     };
   }];
+
+  # Wallpaper par défaut GNOME
+  programs.dconf.profiles.user.databases = [{
+    settings = {
+      "org/gnome/desktop/background" = {
+        picture-uri       = "file:///run/current-system/sw/share/backgrounds/roudix/roudix-light.svg";
+        picture-uri-dark  = "file:///run/current-system/sw/share/backgrounds/roudix/roudix-dark.svg";
+        picture-options   = "zoom";
+      };
+      "org/gnome/desktop/screensaver" = {
+        picture-uri = "file:///run/current-system/sw/share/backgrounds/roudix/roudix-dark.svg";
+      };
+    };
+  }];
+
+  # Wallpaper par défaut KDE
+  environment.etc."xdg/plasma-workspace/env/roudix-wallpaper.sh" = {
+    mode = "0755";
+    text = ''
+      #!/bin/sh
+      WALLPAPER_DARK="/run/current-system/sw/share/backgrounds/roudix/roudix-dark.svg"
+      WALLPAPER_LIGHT="/run/current-system/sw/share/backgrounds/roudix/roudix-light.svg"
+      CONFIG="$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
+      if [ ! -f "$CONFIG" ]; then
+        mkdir -p "$HOME/.config"
+        cat > "$CONFIG" << KDEEOF
+[Containments][1][Wallpaper][org.kde.image][General]
+Image=$WALLPAPER_DARK
+KDEEOF
+      fi
+    '';
+  };
 }
--- pkgs/roudix-branding/default.nix
+++ pkgs/roudix-branding/default.nix
@@ -21,7 +21,42 @@ stdenvNoCC.mkDerivation {
       convert $src/logo/roudix-logo.png \
         -resize ''${SIZE}x''${SIZE} \
         $out/share/icons/hicolor/''${SIZE}x''${SIZE}/apps/roudix-logo.png
+        # Logo principal
+        convert $src/logo/roudix-logo.png \
+          -resize ''${SIZE}x''${SIZE} \
+          $out/share/icons/hicolor/''${SIZE}x''${SIZE}/apps/roudix-logo.png
+
+        # Icône menu KDE
+        convert $src/logo/roudix-logo.png \
+          -resize ''${SIZE}x''${SIZE} \
+          $out/share/icons/hicolor/''${SIZE}x''${SIZE}/apps/start-here-kde.png
+
+        convert $src/logo/roudix-logo.png \
+          -resize ''${SIZE}x''${SIZE} \
+          $out/share/icons/hicolor/''${SIZE}x''${SIZE}/apps/start-here.png
     done
+    # Wallpapers
+    mkdir -p $out/share/backgrounds/roudix
+    cp $src/wallpaper/roudix-dark.svg $out/share/backgrounds/roudix/roudix-dark.svg
+    cp $src/wallpaper/roudix-light.svg $out/share/backgrounds/roudix/roudix-light.svg
+
+    # Entrées GNOME background properties (paire dark/light)
+    mkdir -p $out/share/gnome-background-properties
+    cat <<XMLEOF > $out/share/gnome-background-properties/roudix.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE wallpapers SYSTEM "gnome-wp-list.dtd">
+<wallpapers>
+  <wallpaper deleted="false">
+    <name>Roudix</name>
+    <filename>/run/current-system/sw/share/backgrounds/roudix/roudix-light.svg</filename>
+    <filename-dark>/run/current-system/sw/share/backgrounds/roudix/roudix-dark.svg</filename-dark>
+    <options>zoom</options>
+    <shade_type>solid</shade_type>
+    <pcolor>#eff1f5</pcolor>
+    <scolor>#1e1e2e</scolor>
+  </wallpaper>
+</wallpapers>
+XMLEOF
   '';
 
   meta = {
