d726d449d09c6da83c96506b4f57aebe7589d23c
roudinebwt <roudinebwt@proton.me>
2026-08-02 04:43:52 +0200

    Scripted auto-commit on change (2026-08-02 04:43:52) by gitwatch.sh

--- hosts/roudix/configuration.nix
+++ hosts/roudix/configuration.nix
@@ -14,6 +14,7 @@
     ../../modules/system/scx.nix
     ../../modules/system/flatpak.nix
     ../../modules/system/gpu
+    ../../modules/system/gpu/undervolt.nix
     ../../modules/system/roudix-rgb.nix
     ../../modules/system/cpu.nix
     ../../modules/system/pipewire.nix
