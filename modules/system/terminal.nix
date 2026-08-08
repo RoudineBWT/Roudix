4c64971e739109938504177463ea0595ffb7e824
roudinebwt <roudinebwt@proton.me>
2026-08-08 22:56:09 +0200

    Scripted auto-commit on change (2026-08-08 22:56:09) by gitwatch.sh

--- modules/system/terminal.nix
+++ modules/system/terminal.nix
@@ -2,7 +2,7 @@
 with lib;
 {
   options.roudix.terminal = mkOption {
-    type = types.enum [ "ghostty" "kitty" "alacritty" "foot" "wezterm" ];
+    type = types.enum [ "ghostty" "kitty" "alacritty" "foot" "wezterm" "ptyxis" "konsole" ];
     default = "ghostty";
     description = "Terminal émulateur par défaut installé et configuré comme terminal principal.";
   };
