e57419c0f20ba55582ebb00e4d6ec19da04fdb59
roudinebwt <roudinebwt@proton.me>
2026-08-13 14:07:44 +0200

    Scripted auto-commit on change (2026-08-13 14:07:44) by gitwatch.sh

--- modules/system/desktop/jay.nix
+++ modules/system/desktop/jay.nix
@@ -22,7 +22,6 @@ config = lib.mkIf isJay {
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
       xdg-desktop-portal-gtk
     ];
     config.common.default = "*";
