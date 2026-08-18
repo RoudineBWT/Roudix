d73ea3970ffb70e62af69e40713384fe5411acc3
roudinebwt <roudinebwt@proton.me>
2026-08-18 18:46:03 +0200

    Scripted auto-commit on change (2026-08-18 18:46:03) by gitwatch.sh

--- modules/system/gpu/undervolt.nix
+++ modules/system/gpu/undervolt.nix
@@ -1,8 +1,16 @@
 { config, pkgs, lib, ... }:
 {
+  options.roudix.undervolt.only-amd.enable = lib.mkOption {
+    description = "Enable Roudix Amd Undervolting configurations";
+    type = lib.types.bool;
+    default = false;
+  };
+
+  config = lib.mkIf config.roudix.undervolt.only-amd.enable {
 
 boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
 environment.systemPackages = [ pkgs.lact ];
 systemd.packages = [ pkgs.lact ];
 systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+    };
 }
