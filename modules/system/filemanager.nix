f53b9939608a50014f0657bae3dcdcd6eb068c71
roudinebwt <roudinebwt@proton.me>
2026-08-19 02:36:06 +0200

    Scripted auto-commit on change (2026-08-19 02:36:06) by gitwatch.sh

---
+++ modules/system/filemanager.nix
@@ -0,0 +1,39 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  # `command` = binaire réellement lancé (utilisé par le bind niri MOD+E).
+  # Les noms d'attributs ci-dessous matchent déjà les vrais binaires.
+  fileManagerDefs = {
+    "nautilus"   = { package = pkgs.nautilus;             command = "nautilus";   extras = [ pkgs.gvfs ]; };
+    "dolphin"    = { package = pkgs.kdePackages.dolphin;   command = "dolphin";    extras = []; };
+    "nemo"       = { package = pkgs.nemo;                  command = "nemo";       extras = [ pkgs.gvfs ]; };
+    "thunar"     = { package = pkgs.xfce.thunar;           command = "thunar";     extras = [ pkgs.gvfs pkgs.xfce.thunar-volman ]; };
+    "pcmanfm-qt" = { package = pkgs.libsForQt5.pcmanfm-qt; command = "pcmanfm-qt"; extras = []; };
+  };
+in
+{
+  options.roudix.fileManager = mkOption {
+    type = types.enum (attrNames fileManagerDefs);
+    default = "nautilus";
+    description = ''
+      Gestionnaire de fichiers par défaut : installé, et lié au bind niri
+      MOD+E ainsi qu'à l'intégration "ouvrir un terminal ici" (nautilus
+      uniquement, via nautilus-open-any-terminal).
+    '';
+  };
+
+  config = {
+    environment.systemPackages =
+      [ fileManagerDefs.${config.roudix.fileManager}.package ]
+      ++ fileManagerDefs.${config.roudix.fileManager}.extras;
+
+    # Nautilus a besoin de son extension pour respecter roudix.terminal.
+    # Les autres file managers ont leur propre mécanisme natif (ou aucun
+    # équivalent packagé actuellement).
+    programs.nautilus-open-any-terminal =
+      mkIf (config.roudix.fileManager == "nautilus") {
+        enable   = true;
+        terminal = config.roudix.terminal;
+      };
+  };
+}
--- modules/system/terminal.nix
+++ modules/system/terminal.nix
@@ -1,21 +1,9 @@
-{ lib, config, ... }:
+{ lib, ... }:
 with lib;
 {
   options.roudix.terminal = mkOption {
     type = types.enum [ "ghostty" "kitty" "alacritty" "foot" "wezterm" "ptyxis" "konsole" ];
     default = "ghostty";
     description = "Terminal émulateur par défaut installé et configuré comme terminal principal.";
-  };
-
-  config = {
-    # "Ouvrir un terminal ici" dans Nautilus lance roudix.terminal au lieu
-    # de gnome-terminal. Toutes les valeurs de l'enum ci-dessus sont
-    # supportées par l'extension (ghostty, kitty, alacritty, foot,
-    # wezterm, ptyxis, konsole).
-    # Ignoré sur KDE : Dolphin n'utilise pas Nautilus/cette extension.
-    programs.nautilus-open-any-terminal = mkIf (config.roudix.desktop.type != "kde") {
-      enable   = true;
-      terminal = config.roudix.terminal;
-    };
   };
 }
