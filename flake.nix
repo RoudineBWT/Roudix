696105af167a701d75221975631848c62cab0749
roudinebwt <roudinebwt@proton.me>
2026-08-22 03:37:44 +0200

    Scripted auto-commit on change (2026-08-22 03:37:44) by gitwatch.sh

--- flake.nix
+++ flake.nix
@@ -192,7 +192,6 @@
           home-manager.extraSpecialArgs = specialArgs;
           home-manager.users.${username} = { ... }: {
             imports = [
-              inputs.umbriel.homeModules.default
               ./home/common.nix
               ./home/niri.nix
               ./home/umbriel/default.nix
