572100a13e5fd5bea4a40a63126c58c300a854f2
roudinebwt <roudinebwt@proton.me>
2026-08-22 03:35:37 +0200

    Scripted auto-commit on change (2026-08-22 03:35:37) by gitwatch.sh

--- modules/system/desktop/umbriel.nix
+++ modules/system/desktop/umbriel.nix
@@ -1,5 +1,4 @@
-# umbriel.nix (système)
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
@@ -8,16 +7,22 @@
 
   programs.umbriel.enable = true;
 
-  # Portals
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        inputs.xdg-desktop-portal-umbriel.packages.${pkgs.stdenv.hostPlatform.system}.default
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-gnome
-      ];
+  # ─── Portals ───────────────────────────────────────────────────────────
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      inputs.xdg-desktop-portal-umbriel.packages.${pkgs.stdenv.hostPlatform.system}.default
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];  # ← Il manquait ce ";"
+    config.umbriel = {
+      default = [ "umbriel" "gtk" "gnome" ];
+      "org.freedesktop.impl.portal.ScreenCast" = [ "umbriel" ];
+      "org.freedesktop.impl.portal.RemoteDesktop" = [ "umbriel" ];
+    };
+  };
 
-  # Vos autres services système (comme gnome-keyring)
+  # ─── Keyring ───────────────────────────────────────────────────────────
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.greetd.enableGnomeKeyring = true;
 }
