3ef91008001947ed5188d9c21b464840e49ddea4
roudinebwt <roudinebwt@proton.me>
2026-08-22 04:02:20 +0200

    Scripted auto-commit on change (2026-08-22 04:02:20) by gitwatch.sh

--- home/umbriel/rules.nix
+++ home/umbriel/rules.nix
@@ -5,14 +5,14 @@
     # ─── Discord ──────────────────────────────────────────────────────────
     {
       match.app_id = "discord";
-      default_workspace = "󰊗";
+      default_workspace = 1;
       default_maximize = true;
     }
 
     # ─── Element ──────────────────────────────────────────────────────────
     {
       match.app_id = "Element";
-      default_workspace = "󰊗";
+      default_workspace = 2;
       default_maximize = true;
     }
 
@@ -25,18 +25,18 @@
     # ─── Telegram ─────────────────────────────────────────────────────────
     {
       match.app_id = "org.telegram.desktop";
-      default_workspace = "󰊗";
+      default_workspace = 3;
     }
 
     # ─── Firefox ──────────────────────────────────────────────────────────
     {
       match.app_id = "^firefox$";
-      default_workspace = "󰈹";
+      default_workspace = 4;
       default_maximize = true;
     }
     {
       match.title = "About Mozilla Firefox";
-      default_workspace = "󰈹";
+      default_workspace = 4;
       default_floating = true;
     }
     {
@@ -48,20 +48,20 @@
     # ─── Zed ──────────────────────────────────────────────────────────────
     {
       match.app_id = "dev.zed.Zed";
-      default_workspace = "󰊗";
+      default_workspace = 5;
       default_maximize = true;
     }
 
     # ─── Zen Browser ──────────────────────────────────────────────────────
     {
       match.app_id = "zen-twilight";
-      default_workspace = "󰈹";
+      default_workspace = 4;
       default_maximize = true;
       opacity = 0.95;
     }
     {
       match.title = "About Zen Twilight";
-      default_workspace = "󰈹";
+      default_workspace = 4;
       default_floating = true;
       opacity = 0.95;
     }
@@ -74,12 +74,12 @@
     # ─── Brave ────────────────────────────────────────────────────────────
     {
       match.app_id = "brave-origin-beta";
-      default_workspace = "󰈹";
+      default_workspace = 4;
       default_maximize = true;
     }
     {
       match.app_id = "brave-browser";
-      default_workspace = "󰊗";
+      default_workspace = 1;
       default_maximize = true;
     }
     {
@@ -96,14 +96,14 @@
     }
     {
       match.app_id = "^org\\.gnome\\.Nautilus$";
-      default_workspace = "󰊗";
+      default_workspace = 6;
       default_maximize = true;
     }
 
     # ─── Steam ────────────────────────────────────────────────────────────
     {
       match.app_id = "steam";
-      default_workspace = "󰊗";
+      default_workspace = 7;
       default_maximize = true;
     }
     {
@@ -113,62 +113,62 @@
     }
     {
       match.title = "Friends List";
-      default_workspace = "󰊗";
+      default_workspace = 7;
       default_floating = true;
     }
 
     # ─── Jeux ─────────────────────────────────────────────────────────────
     {
       match.app_id = "^(steam_app_.*)$";
-      default_workspace = "󰊗";
+      default_workspace = 7;
       default_fullscreen = true;
     }
     {
       match.app_id = "^heroic$";
-      default_workspace = "󰊗";
+      default_workspace = 7;
       default_fullscreen = true;
     }
     {
       match.app_id = "org.prismlauncher.PrismLauncher";
-      default_workspace = "󰊗";
+      default_workspace = 7;
       default_maximize = true;
     }
     {
       match.app_id = "Minecraft";
-      default_workspace = "󰊗";
+      default_workspace = 7;
       default_fullscreen = true;
     }
 
     # ─── Autres ────────────────────────────────────────────────────────────
     {
       match.app_id = "openrgb";
-      default_workspace = "󰊗";
+      default_workspace = 7;
     }
     {
       match.app_id = "kitty";
-      default_workspace = "󰊗";
+      default_workspace = 5;
       default_floating = true;
     }
     {
       match.app_id = "org.gnome.Ptyxis";
-      default_workspace = "󰊗";
+      default_workspace = 5;
     }
     {
       match.app_id = "org.gnome.TextEditor";
-      default_workspace = "󰊗";
+      default_workspace = 6;
     }
     {
       match.app_id = "Spotify";
-      default_workspace = "󰊗";
+      default_workspace = 8;
       default_maximize = true;
     }
     {
       match.app_id = "com.kde.easyeffects";
-      default_workspace = "󰊗";
+      default_workspace = 8;
     }
     {
       match.app_id = "com.github.wwmm.easyeffects";
-      default_workspace = "󰊗";
+      default_workspace = 8;
     }
   ];
 }
