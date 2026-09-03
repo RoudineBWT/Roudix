30652afa0e87548a8007949763de5860381bfa17
roudinebwt <roudinebwt@proton.me>
2026-09-03 22:47:13 +0200

    Scripted auto-commit on change (2026-09-03 22:47:13) by gitwatch.sh

--- modules/home/desktop/niri/_ws.nix
+++ modules/home/desktop/niri/_ws.nix
@@ -4,10 +4,10 @@
 { }: {
   web = "󰈹";  # U+F0239
   code = "";  # U+0E8DA
-  chat = "";  # U+0F1FF
   term = "";  # U+0E795
   games = "󰊗";  # U+F0297
   files = "󰉋";  # U+F024B
+  chat = "";  # U+0F1FF
   music = "󰝚";  # U+F075A
   browser2 = "";  # U+0E743
 }
