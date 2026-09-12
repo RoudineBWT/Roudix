ee4ea81531880af2cc490697569f4f8603734109
roudinebwt <roudinebwt@proton.me>
2026-09-12 22:09:33 +0200

    Scripted auto-commit on change (2026-09-12 22:09:33) by gitwatch.sh

--- hosts/roudix/configuration.nix
+++ hosts/roudix/configuration.nix
@@ -3,7 +3,6 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/system/shell.nix
-    ../../modules/system/nixpkgs-compat.nix
     ../../modules/system/autoupdate.nix
     ../../modules/system/common.nix
     ../../modules/system/desktop
