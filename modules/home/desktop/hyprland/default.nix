f400215d3a3c80abc6136ed7c1dad2ba7851be0d
roudinebwt <roudinebwt@proton.me>
2026-09-26 00:11:38 +0200

    Scripted auto-commit on change (2026-09-26 00:11:38) by gitwatch.sh

--- modules/home/desktop/hyprland/default.nix
+++ modules/home/desktop/hyprland/default.nix
@@ -3,7 +3,6 @@ let
   shellType = osConfig.roudix.desktop.shell or "noctalia";
   hyprFiles = [
     "hyprland.lua"
-    "README.md"
     "config/animations.lua"
     "config/autostart.lua"
     "config/colors.lua"
