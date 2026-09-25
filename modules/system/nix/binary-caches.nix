cd754bb37695e33ba33acad88835772199e8dacb
roudinebwt <roudinebwt@proton.me>
2026-09-25 02:10:13 +0200

    Scripted auto-commit on change (2026-09-25 02:10:13) by gitwatch.sh

--- modules/system/nix/binary-caches.nix
+++ modules/system/nix/binary-caches.nix
@@ -3,13 +3,25 @@
   nix.settings = {
     extra-substituters = [
       "https://attic.xuyh0120.win/lantian"
-      "https://cache.garnix.io"
       "https://noctalia.cachix.org"
+      "https://prismlauncher.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://roudix.cachix.org"
+      "https://nix-cache.tokidoki.dev/tokidoki"
+      "https://nyx-cache.chaotic.cx/"
+      "https://niri-epireyn.cachix.org"
+      "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "niri-epireyn.cachix.org-1:tlVyFN7CtsDT+ZcLPS+ekFWeT1X6X4OqvWqbBMyIzFA="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+      "prismlauncher.cachix.org-1:9/n/FGyABA2jLUVfY+DEp4hKds/rwO+SCOtbOkDzd+c="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSeBw="
+      "roudix.cachix.org-1:h5EnhsXw4Mr6pLUpZIalE8SlfH1kKXgvPFvl+yrTAaQ="
+      "tokidoki:MD4VWt3kK8Fmz3jkiGoNRJIW31/QAm7l1Dcgz2Xa4hk="
+      "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 }
